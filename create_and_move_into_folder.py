027b8c52abaddf260b8b2f6877f640966ac22be2
TheNotoriousMMA <kevin6090996140@gmail.com>
2022-07-02 19:56:27 +0800

    Create create_and_move_into_folder.py

---
+++ create_and_move_into_folder.py
@@ -0,0 +1,19 @@
+"""
+將資料夾中所有檔案分別建立並移至該資料夾
+"""
+import os
+import shutil  
+from pathlib import Path
+
+current_path = Path(__file__)
+parent_dir = current_path.parent.absolute()
+files = os.listdir(parent_dir)
+
+for file in files:
+    if (file.endswith(".git")) or (file.endswith("README.md")):
+        continue
+    else:
+        print(file)
+        folder_name = Path(file).resolve().stem
+        os.mkdir(Path(parent_dir).joinpath(folder_name))
+        shutil.move(Path(parent_dir).joinpath(file).as_posix() , Path(parent_dir).joinpath(folder_name).as_posix() )
